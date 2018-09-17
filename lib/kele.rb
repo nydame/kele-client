4c23c7dc82559334890a1ca0528e10504febdddf
felicia <felicia@hyperarts.com>
2018-09-17 11:45:53 -0700

    Write new method get_me.

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,12 +25,23 @@ class Kele
     end
 
     if @response.message == "OK"
-      # what to do with token; set cookie???
+      # what to do with token; set cookie??? I AM STILL NOT LOGGED IN!
       # display success message
       puts "You successfully logged into Bloc."
     else
       # raise error
       puts "Login attempt failed: #{@response.message}."
+    end
+  end
+
+  def get_me
+    begin
+      auth_header = { authorization: @response['auth_token'] }
+      current_user_response = self.class.get('/users/me', headers: auth_header)
+    rescue HTTParty::Error
+      puts "Requested data could not be sent: #{current_user_response.message}."
+    rescue => e
+      puts "Something went wrong: #{e.message}."
     end
   end
 
