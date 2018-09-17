2256956acb5fd8954e36a1e6bbee2aece7d98cd0
felicia <felicia@hyperarts.com>
2018-09-17 10:09:37 -0700

    Initialization and authorization: completed with revisions.

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,20 +2,36 @@ require 'httparty'
 require 'pp'
 
 class Kele
-  include HTTParty
+  attr_accessor :options, :response
 
+  include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
   format :json
 
   def initialize(email, password)
-    @email = email
-    @password = password
-    @auth_token = self.class.post('/sessions', {
-      body: {
-        email: @email,
-        password: @password
-      }
+    @options = {
+      email: email,
+      password: password,
+    }
+    @response = nil
+
+    begin
+      @response = self.class.post('/sessions', {
+        body: @options
       })
+    rescue => e
+      puts "Login attempt failed: #{e.message}."
+      return
+    end
+
+    if @response.message == "OK"
+      # what to do with token; set cookie???
+      # display success message
+      puts "You successfully logged into Bloc."
+    else
+      # raise error
+      puts "Login attempt failed: #{@response.message}."
+    end
   end
 
 end
