2cd36b48870acd4a7e0ef0833149b9d9d5c6cdcd
felicia <felicia@hyperarts.com>
2018-09-19 16:52:53 -0700

    Checkpoint 5 - Roadmaps and Checkpoints: added error handling to get_checkpoints method.

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -15,10 +15,16 @@ module Roadmap
   end
 
   def get_checkpoint(checkpoint_id)
-    auth_header = {authorization: @response['auth_token']}
-    checkpoint_response = self.class.get("/checkpoints/#{checkpoint_id}", headers: auth_header)
-    checkpoint_json = checkpoint_response.body
-    checkpoint_hash = Oj.load(checkpoint_json)
+    begin
+      auth_header = {authorization: @response['auth_token']}
+      checkpoint_response = self.class.get("/checkpoints/#{checkpoint_id}", headers: auth_header)
+      checkpoint_json = checkpoint_response.body
+      checkpoint_hash = Oj.load(checkpoint_json)
+    rescue HTTParty::Error
+      puts "Requested data could not be sent: #{checkpoint_response.message}."
+    rescue => e
+      puts "Something went wrong: #{e.message}."
+    end
   end
 
 end
