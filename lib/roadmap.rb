256cea1bf50e651926ac7096dceccc97ef37fbd8
felicia <felicia@hyperarts.com>
2018-09-21 12:15:43 -0700

    Checkpoint 7 - Remaining Checkpoints: completed.

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -15,12 +15,23 @@ module Roadmap
 
   def get_checkpoint(checkpoint_id)
     begin
-      auth_header = {authorization: @response['auth_token']}
       checkpoint_response = self.class.get("/checkpoints/#{checkpoint_id}", headers: auth_header)
       checkpoint_json = checkpoint_response.body
       checkpoint_hash = Oj.load(checkpoint_json)
     rescue HTTParty::Error
       puts "Requested data could not be sent: #{checkpoint_response.message}."
+    rescue => e
+      puts "Something went wrong: #{e.message}."
+    end
+  end
+
+  def get_remaining_checkpoints(chain_id)
+    begin
+      remaining_checkpoint_response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: auth_header)
+      remaining_checkpoint_json = remaining_checkpoint_response.body
+      remaining_checkpoint_hash = Oj.load(remaining_checkpoint_json)
+    rescue HTTParty::Error
+      puts "Requested data could not be sent: #{remaining_checkpoint_response.message}."
     rescue => e
       puts "Something went wrong: #{e.message}."
     end
